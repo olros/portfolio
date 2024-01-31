dbe8024f857a6ad87cf66d05da4b39b9e092ae78
Olaf Rosendahl <olafrosendahl@gmail.com>
2024-01-31 20:13:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        sanntid: './personvern/sanntid.html',
+      },
+    },
+  },
 });
